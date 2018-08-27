b65e9d43a25f3945a527f1a637549e49eb9560d7
tz925 <sai950925@hotmail.com>
2018-08-27 17:07:11 -0400

    fixing

--- routes/locations.js
+++ routes/locations.js
@@ -16,20 +16,40 @@ router.get('/', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
   // console.log(req.body);
-  const {name, area, position, info, buyingPower} = req.body
+  const {name, area, position, info, buyingPower, dailyView} = req.body
   const location = new Location({
     _id: new mongoose.Types.ObjectId(),
     name,
     area,
     position,
     info,
-    buyingPower
+    buyingPower,
+    dailyView
   })
   location.save().then(result => {
     console.log(result)
     res.status(200).json(result)
   })
   .catch(err => console.log(err))
+})
+
+router.post('/populate', (req, res, next) => {
+  const list = req.body.list
+  console.log(list);
+  list.forEach(async (location) => {
+    const {name, area, position, info, buyingPower, dailyView} = location;
+    const newLocation = new Location({
+      _id: new mongoose.Types.ObjectId(),
+      name,
+      area,
+      position,
+      info,
+      buyingPower,
+      dailyView
+    })
+    await newLocation.save().then(result => console.log("good")).catch(err => console.log(err))
+  })
+  res.status(200).send("Okay!")
 })
 
 module.exports = router;
