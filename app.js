5101f214caba5d772d5f210e00d808fefa997ddb
tz925 <sai950925@hotmail.com>
2018-08-27 12:53:27 -0400

    setting up node API server

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+const locationRoutes = require('./routes/locations');
+app.use('/locations', locationRoutes)
+
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status( err.code || 500 )
+    .json({
+      status: 'error',
+      message: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500)
+  .json({
+    status: 'error',
+    message: err.message
+  });
+});
+module.exports = app;
---
+++ routes/locations.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message:"hadnleing get req to /locations"
+  })
+})
+
+router.post('/', (req, res, next) => {
+  console.log(req.body);
+})
+
+module.exports = router;
