f5662e043a593547d12ea82449ad54527ab9a9f6
tz925 <sai950925@hotmail.com>
2018-08-28 18:27:09 -0400

    added cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 const locationRoutes = require('./routes/locations');
 
@@ -25,6 +26,7 @@ app.use((req, res, next) => {
   }
   next();
 })
+app.use(cors())
 
 app.use('/locations', locationRoutes)
 
